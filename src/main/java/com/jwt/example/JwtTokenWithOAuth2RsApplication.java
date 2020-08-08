311cf7cd093332061f704c1333fe9507295a3a89
Peggy-liu <101808315@student.swin.edu.au>
2020-08-08 15:59:44 +1000

    first commit

---
+++ src/main/java/com/jwt/example/JwtTokenWithOAuth2RsApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenWithOAuth2RsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTokenWithOAuth2RsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jwt/example/config/ResourceServer.java
@@ -0,0 +1,58 @@
+package com.jwt.example.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+
+@Configuration
+@EnableResourceServer
+public class ResourceServer extends ResourceServerConfigurerAdapter {
+
+	
+	@Value("${key}")
+	private String publicKey;
+	
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(converter());
+	}
+	
+	
+	/*
+	 * JWT Symmetric key implementation
+	 */
+	
+//	@Bean
+//	public JwtAccessTokenConverter converter() {
+//		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//		converter.setSigningKey("secret");
+//		return converter;
+//	}
+	
+	/*
+	 * JWT Asymmetric key implementation
+	 */
+	
+	@Bean
+	public JwtAccessTokenConverter converter() {
+		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		converter.setVerifierKey(publicKey);
+		return converter;
+	}
+	
+	
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore());
+	}
+	
+	
+}
